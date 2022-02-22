e2f80d71138043430df85cc8cf160b92463a4cfa
fawaz-exe <72291838+fawaz-exe@users.noreply.github.com>
2022-02-22 21:16:27 +0530

    Add files via upload

---
+++ 15th_feb_assignments/20.js
@@ -0,0 +1,24 @@
+// Write a program to check if given number is even or not
+// a = 2;
+// if((a%2) != 0)
+// console.log('Odd')
+
+// if((a%2) == 0){
+//     console.log('Even')
+// }
+
+// // else is for rest all, avoid unnessacry execution
+
+// if ((a%2) !=0 ){
+//     console.log(' it is an odd number')
+// } else if ((a%2) == 0){
+//     console.log('It is an even number')
+// } else {
+//     console.log('neither nor odd')
+// }
+
+// Ternary
+
+var age = 10;
+
+(age > 12) ? ((age < 20) ? console.log('I am teen') : console.log('I am an adult')) : console.log('I am a kid');
---
+++ 15th_feb_assignments/21.js
@@ -0,0 +1,47 @@
+//Data Types
+/*
+Primitive DataTypes:
+    Number
+    String
+    Boolean
+    Null
+    Undefined
+    Symbol
+    BigInt
+
+    Function
+    Object
+    Array 
+    
+    Function - method
+    Variable-Identifier-Attributes
+    parameter-argument
+    
+    How to check data type of any variable*/
+
+        // var a = 10;
+        // // console.log(typeof(a));
+        // // console.log(typeof(0.1+0.2))
+        // console.log(typeof(a+3))
+        // console.log(typeof(a-3))
+        // console.log(typeof(a*3))
+        // console.log(typeof(a/3))
+        // console.log(typeof(a%3))
+        // console.log(a/0)
+        // console.log(typeof(a/0))
+
+        //String
+        // var a = 'Hello'
+        // var b = 'there'
+        // console.log(a+b)
+        // //String Literal
+            var a = 'akhil'
+            console.log(`'Hi'${a} 'How are you?'`);
+
+        //Undefined
+        // var b;
+        // console.log(typeof(b))
+        //Null
+
+        //NAN
+
